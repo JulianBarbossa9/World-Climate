70bbe19e067a67bd596e524ccbb17f45acceedba
JulianBarbossa9 <julianbarbossa@hotmail.com>
2021-11-28 16:12:18 -0500

    handleChange

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+    
+    const [ search, keepSearch ] = useState({
+        city:'',
+        country:''
+    });
+
+    const { city , country} = search; 
+
+    //HandleChange -- función que coloca los elementos del onchange 
+    const handleChange = (e) => {
+        keepSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        });
+    }
+    
+    
     return (  
         <form>
             <div className="input-field col s12">
@@ -8,7 +25,9 @@ const Form = () => {
                     type="text"
                     name="city"
                     id="city"
-                />
+                    value={city}
+                    onClick={handleChange}
+                    />
                 <label htmlFor="city">City: </label>
             </div>
 
@@ -16,8 +35,17 @@ const Form = () => {
                 <select
                     name="country"
                     id="country"
+                    value={country}
+                    onClick={handleChange}
                 >
                     <option value="">-- Select country --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="country">Country: </label>
             </div>
