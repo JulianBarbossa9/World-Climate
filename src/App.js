ce48c0afbddc68b67936a1d06b434f8783414de9
JulianBarbossa9 <julianbarbossa@hotmail.com>
2021-11-28 15:15:16 -0500

    create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
 
 
 
 function App() {
   return (
-    <h2>World Climate</h2>
+    <Fragment>
+      <Header
+        title = "World Climate"
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+              <div className="col m6 s12">
+                  <Form 
+                    
+                  />
+              </div>
+              <div className="col m6 s12">
+                  2
+              </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
